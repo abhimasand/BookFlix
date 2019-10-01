0a956b34bf819f506668a4764a1fe5a45804a080
Tanish Grover <tanigrover@gmail.com>
2019-10-01 16:41:26 +0530

    Added book selector feature

--- books/static/js/script.js
+++ books/static/js/script.js
@@ -1,11 +1,39 @@
-$(function() {
-  $("#selectable").selectable({
-    selected: function() {
-      var selectedItemList = $("#selected-item-list").empty();
-      $(".ui-selected", this).each(function() {
-        var index = $("#selectable img").index(this);
-        selectedItemList.append((index + 1) + ", ");
-      });
-    }
-  });
-});
+var selectables = document.querySelectorAll("#selectables .item");
+var submitSelected = document.querySelector("#submitSelected");
+
+var selected = [];
+
+for (var i = 0; i < selectables.length; i++) {
+	selectables[i].addEventListener("click", handleSelect);
+}
+
+function handleSelect() {
+	console.log(this.id);
+	if (this.classList.contains("selected")) {
+		for (var i = 0; i < selected.length; i++) {
+			if (selected[i] === this.id) {
+				selected.splice(i, 1);
+				break;
+			}
+		}
+	} else {
+		selected.push(this.id);
+	}
+
+	this.classList.toggle("selected");
+	console.log(selected);
+}
+
+submitSelected.onclick = function() {
+	$.ajax({
+		type: "POST",
+		url: "/read_books/handle_selected_books",
+		data: {
+			"selected[]": selected
+		},
+		success: function(data) {
+			console.log("success: ", data);
+			// Redirect here
+		}
+	});
+};
